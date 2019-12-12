fa929dbefaaf7fcf80f5ef2b9b566c5957e6f543
dani <daniel.soto.j@gmail.com>
2019-12-12 00:56:07 -0700

    Included libraries and added one function for findinf industry describtion. Non of the Function/Methods? did not work. Even though I created an object MU with the class python does not recognize MU as a string but as an object that cannot concatenate itself into a url

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import re #For regular expression
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup #For making html file prety
+import ssl #foe error handling
+
+
 class StockTicker:
     def html_retrieve(self):
         fhand = open('.txt', 'w')   #The 'w' argument erases content of text
@@ -5,11 +11,31 @@ class StockTicker:
         url = 'https://www.marketwatch.com/investing/stock/'+self+'/profile'
         html = urllib.request.urlopen(url, context=ctx).read()
         soup = BeautifulSoup(html, 'html.parser')
-            tags = soup('p') #May be any tag
-            for tag in tags:
-                try:
-                    fhand.write(tag.string)
-                except:
-                    continue
+        tags = soup('p') #May be any tag
+        for tag in tags:
+            try:
+                fhand.write(tag.string)
+            except:
+                continue
             #print(tags) #for troubleshooting
             fhand.close()
+
+    def industry(self):
+        hand = open('.txt',)
+        cnt = 0
+        for line in hand:
+            #print(line)
+            line = line.lstrip()
+            cnt = cnt + 1
+            #print('^^^^^^^^^^^^^'+line,cnt)
+            #biolist= re.findall('^CenturyLink, Inc([+a-z0-9])',line)
+            bio_exist = False
+            if cnt == 19:
+                bio = line
+                bio_exist = True
+                break
+            if bio_exist == False:
+                bio = None
+        if bio != None:
+            print('==========='+'Industry'+'==========='+'\n\n'+bio)
+        return(bio)
