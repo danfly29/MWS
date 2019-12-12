958925a5878987db42de34176e1dea3128f6992c
dani <daniel.soto.j@gmail.com>
2019-12-12 00:34:20 -0700

    Created a StockTicker Class with a method for retrieving the <p> tags from the marketwatch profile into a text file that will be recycled.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def html_retriever():
-    pass
-
-def sector_giver():
-    pass
-
-def financial_ratios():#Should contain the regular expressions for pulling from
-    pass               #profile
+class StockTicker:
+    def html_retrieve(self):
+        fhand = open('.txt', 'w')   #The 'w' argument erases content of text
+                                    #file and avoids clutter when text files has no name
+        url = 'https://www.marketwatch.com/investing/stock/'+self+'/profile'
+        html = urllib.request.urlopen(url, context=ctx).read()
+        soup = BeautifulSoup(html, 'html.parser')
+            tags = soup('p') #May be any tag
+            for tag in tags:
+                try:
+                    fhand.write(tag.string)
+                except:
+                    continue
+            #print(tags) #for troubleshooting
+            fhand.close()
