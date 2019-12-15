e244e5c5ae1930f5907647a9c60b980d4072faa4
dani <daniel.soto.j@gmail.com>
2019-12-15 00:02:04 -0700

    Made a class (StockTicker) with the method load that creates the atributes .name .industry .html.     Note: This version of the html_retrieve function does not use a file to store tags

--- main.py
+++ main.py
@@ -4,38 +4,57 @@ from bs4 import BeautifulSoup #For making html file prety
 import ssl #foe error handling
 
 
-class StockTicker:
-    def html_retrieve(self):
-        fhand = open('.txt', 'w')   #The 'w' argument erases content of text
-                                    #file and avoids clutter when text files has no name
-        url = 'https://www.marketwatch.com/investing/stock/'+self+'/profile'
-        html = urllib.request.urlopen(url, context=ctx).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        tags = soup('p') #May be any tag
-        for tag in tags:
-            try:
-                fhand.write(tag.string)
-            except:
-                continue
-            #print(tags) #for troubleshooting
-            fhand.close()
 
-    def industry(self):
-        hand = open('.txt',)
-        cnt = 0
-        for line in hand:
-            #print(line)
+#Main Functions
+def html_retrieve(self):
+    ls = []
+    url = 'https://www.marketwatch.com/investing/stock/'+self+'/profile'
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('p') #May be any tag
+    for tag in tags:
+        try:
+            ls.append(tag.string)
+        except:
+            continue
+        #print(tags) #for troubleshooting
+    return(ls)
+
+
+def industry(self):
+    cnt = 0
+    for line in self:
+        print(line)
+        try:
             line = line.lstrip()
-            cnt = cnt + 1
-            #print('^^^^^^^^^^^^^'+line,cnt)
-            #biolist= re.findall('^CenturyLink, Inc([+a-z0-9])',line)
-            bio_exist = False
-            if cnt == 19:
-                bio = line
-                bio_exist = True
-                break
-            if bio_exist == False:
-                bio = None
+        except:
+            continue
+        cnt = cnt + 1
+        print('^^^^^^^^^^^^^'+line,cnt)
+        #biolist= re.findall('^CenturyLink, Inc([+a-z0-9])',line)
+        bio_exist = False
+        if cnt == 22:
+            bio = line
+            bio_exist = True
+            break
+        if bio_exist == False:
+            bio = None
         if bio != None:
             print('==========='+'Industry'+'==========='+'\n\n'+bio)
-        return(bio)
+    return(bio)
+
+
+class StockTicker:
+    def load(self,i):
+        self.name = i
+        self.html = html_retrieve(i)
+        self.industry = industry(self.html)
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+n = StockTicker()
+n.load("MU")
